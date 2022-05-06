6c122bb24f26a47ef802d9b1abdc6a0571dd2f90
Tristen Everett <spock214@gmail.com>
2022-05-06 14:56:04 -0700

    update component to allow functionality instead of just static buttons

--- src/components/HangmanGuesses.js
+++ src/components/HangmanGuesses.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import CustomButton from "./CustomButton";
 
 function HangmanGuesses(props){
@@ -9,21 +10,27 @@ function HangmanGuesses(props){
     gridTemplateColumns: unit.repeat(13)
   };
 
-  const letters = ["A", "B" ,"C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  let buttonArray = [{value: "A", disabled: true}, {value: "B", disabled: true}];
-  for(let i = 2; i < letters.length; i++){
-    buttonArray.push({value: letters[i], disabled: false});
-  }
-
   return(
     <React.Fragment>
       <div style={table}>
-        {buttonArray.map((buttonData) => 
-          <CustomButton display={buttonData.value} disabled={buttonData.disabled} key={buttonData.value} />
+        {props.buttonArray.map((buttonData) => 
+          <div className="guessButton">
+            <CustomButton display={buttonData.value} disabled={buttonData.disabled} onClick={() => props.guessFunc(buttonData.value)} key={buttonData.value} />
+          </div>
         )}
+      </div>
+      <div>
+        <br />
+        <CustomButton display="End Game" disabled={false} onClick={props.gameOver} />
       </div>
     </React.Fragment>
   )
+}
+
+HangmanGuesses.propTypes = {
+  buttonArray: PropTypes.array,
+  guessFunc: PropTypes.func,
+  gameOver: PropTypes.func
 }
 
 export default HangmanGuesses;
